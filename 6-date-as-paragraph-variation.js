76a18de4af675fe54bdcaa67b95987552a453e58
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2024-11-30 15:08:28 +0000

    Add warning about Block Bindings bug

--- 6-date-as-paragraph-variation.js
+++ 6-date-as-paragraph-variation.js
@@ -1,3 +1,33 @@
+/**
+ *
+ * NOTE: This variation doesn't work as of WordPress 6.7, nor Gutenberg 19.8.
+ * The reason is that block bindings, as of their current implementation, are
+ * implemented using standard WordPress hooks, making them inaccessible to
+ * third parties which are themselves using the `editor.BlockEditor` hook. This
+ * will be fixed once the following pull request is merged:
+ *
+ * https://github.com/WordPress/gutenberg/pull/67370
+ *
+ * For this talk, I made this work by using a custom Gutenberg build which
+ * includes this fix. Sorry for not mentioning it during the talk!
+ *
+ */
+
+console.warn(
+`NOTE: The 'Date as a Paragraph' variation doesn't work as of WordPress 6.7,
+nor Gutenberg 19.8. The reason is that block bindings, as of their current
+implementation, are implemented using standard WordPress hooks, making them
+inaccessible to third parties which are themselves using the
+'editor.BlockEditor' hook. This will be fixed once the following pull request
+is merged:
+
+https://github.com/WordPress/gutenberg/pull/67370
+
+For this talk, I made this work by using a custom Gutenberg build which
+includes this fix. Sorry for not mentioning it during the talk!`
+);
+
+
 import { addFilter } from '@wordpress/hooks';
 import {
 	parseWithAttributeSchema,
